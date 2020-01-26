81377968b65371d68617b12e1f7dcac5b1fa4355
Gyan Lakhwani <gyanlakhwani@gmail.com>
2020-01-26 16:10:41 +0530

    Random from Book

---
+++ random-from-book.py
@@ -0,0 +1,22 @@
+## Find freq dist of book
+
+import nltk
+from nltk import *
+
+f = open('constitution.txt', 'r')
+raw = f.read().decode('utf8')
+
+tokens = word_tokenize(raw)
+mybook = nltk.Text(tokens)
+
+def generate_model(cfdist, word, num=15):
+    for i in range(num):
+        print(word + " ")
+        word = cfdist[word].max()
+
+## Print bigrams of book.
+bigrams = nltk.bigrams(mybook)
+cfd = nltk.ConditionalFreqDist(bigrams)
+
+print(cfd['living'])
+print(generate_model(cfd, 'living'))
